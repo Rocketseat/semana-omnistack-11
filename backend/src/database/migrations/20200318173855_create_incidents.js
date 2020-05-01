10ca1e103e0c77c987ad2bb38763660044f20cb8
Joe Yang <jose.ozelzo10@gmail.com>
2020-05-01 13:46:56 -0300

    Atributo link_info para receber links para mais informações nos incid… …

--- backend/src/database/migrations/20200318173855_create_incidents.js
+++ backend/src/database/migrations/20200318173855_create_incidents.js
@@ -5,7 +5,8 @@ exports.up = function(knex) {
     table.string('title').notNullable();
     table.string('description').notNullable();
     table.decimal('value').notNullable();
-
+    table.string('link_info').notNullable();
+    
     table.string('ong_id').notNullable();
 
     table.foreign('ong_id').references('id').inTable('ongs');
